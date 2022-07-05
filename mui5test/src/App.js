5848bf5cad639dd4bde855d7a02e9434cd25c738
郵差 <twHenryYang@gmail.com>
2022-07-05 15:18:01 +0800

    feat: Button with many styles & icon

--- mui5test/src/App.js
+++ mui5test/src/App.js
@@ -1,7 +1,31 @@
+import Button from '@mui/material/Button'
+// https://mui.com/zh/material-ui/getting-started/usage/
+
+import { Settings, Delete, Send } from '@mui/icons-material';
+// https://mui.com/zh/material-ui/icons
+// https://mui.com/material-ui/material-icons//
+
 function App() {
   return (
     <div>
-      Hello MUI
+      Hello MUI<br />
+      {/* https://mui.com/zh/material-ui/react-button/#basic-button */}
+      <Button variant="text">Text Button</Button>
+      <Button variant="contained">Contained Button</Button>
+      <Button variant="outlined">Outlined Button</Button>
+
+      <Button variant="contained" color="success">Success Color</Button> <br />
+      <Button variant="contained" disabled>Disabled</Button>
+
+      <Button variant="contained" color="secondary" startIcon={<Settings />}>
+        Setting
+      </Button>
+      <Button variant="outlined" startIcon={<Delete />}>
+        Delete
+      </Button>
+      <Button variant="contained" endIcon={<Send />}>
+        Send
+      </Button>
     </div>
   );
 }
