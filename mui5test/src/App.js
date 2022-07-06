eaae14eebc9fe8a8438ccf8aee28c0446659ef25
郵差 <twHenryYang@gmail.com>
2022-07-06 10:34:47 +0800

    feat: change sidebar contents

--- mui5test/src/App.js
+++ mui5test/src/App.js
@@ -1,8 +1,6 @@
 import Button from '@mui/material/Button'
 // https://mui.com/zh/material-ui/getting-started/usage/
-import { Typography } from '@mui/material'
 // https://mui.com/zh/material-ui/api/typography/
-import { Settings, Delete, Send } from '@mui/icons-material';
 // https://mui.com/zh/material-ui/icons
 // https://mui.com/material-ui/material-icons//
 import ResponsiveDrawer from './components/ResponsiveDrawer';
@@ -10,28 +8,6 @@ import ResponsiveDrawer from './components/ResponsiveDrawer';
 function App() {
   return (
     <div>
-      Hello MUI<br />
-      {/* https://mui.com/zh/material-ui/react-button/#basic-button */}
-      <Button variant="text">Text Button</Button>
-      <Button variant="contained">Contained Button</Button>
-      <Button variant="outlined">Outlined Button</Button>
-
-      <Button variant="contained" color="success">Success Color</Button> <br />
-      <Button variant="contained" disabled>Disabled</Button>
-
-      <Button variant="contained" color="secondary" startIcon={<Settings />}>
-        Setting
-      </Button>
-      <Button variant="outlined" startIcon={<Delete />}>
-        Delete
-      </Button>
-      <Button variant="contained" endIcon={<Send />}>
-        Send
-      </Button>
-
-      <Typography variant="h3" component="p">
-        It's use h3 style but it's a p tag :)
-      </Typography>
       <ResponsiveDrawer>
       </ResponsiveDrawer>
     </div>
--- mui5test/src/components/ResponsiveDrawer.js
+++ mui5test/src/components/ResponsiveDrawer.js
@@ -1,24 +1,27 @@
 // https://mui.com/zh/material-ui/react-drawer/#responsive-drawer
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import SchoolIcon from '@mui/icons-material/School';
+import QuizIcon from '@mui/icons-material/Quiz';
+import AddReactionIcon from '@mui/icons-material/AddReaction';
+import HistoryIcon from '@mui/icons-material/History';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import ChatIcon from '@mui/icons-material/Chat';
 
-const drawerWidth = 240;
+const drawerWidth = 180;
 
 function ResponsiveDrawer(props) {
     const { window } = props;
@@ -30,14 +33,16 @@ function ResponsiveDrawer(props) {
 
     const drawer = (
         <div>
-            <Toolbar />
+            <Toolbar
+                sx={{color: 'white', backgroundColor: 'darkblue'}}
+            />
             <Divider />
             <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                {['知識寶盒', '牛刀小試', '情境模擬'].map((text, index) => (
                     <ListItem key={text} disablePadding>
                         <ListItemButton>
                             <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                {index === 0 ? <SchoolIcon /> : (index === 1 ? <QuizIcon/> : <AddReactionIcon />)}
                             </ListItemIcon>
                             <ListItemText primary={text} />
                         </ListItemButton>
@@ -46,11 +51,11 @@ function ResponsiveDrawer(props) {
             </List>
             <Divider />
             <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                {['歷史測驗', '我的書籤', '討論交流'].map((text, index) => (
                     <ListItem key={text} disablePadding>
                         <ListItemButton>
                             <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                {index === 0 ? <HistoryIcon /> : (index === 1 ? <BookmarkIcon /> : <ChatIcon />)}
                             </ListItemIcon>
                             <ListItemText primary={text} />
                         </ListItemButton>
@@ -83,7 +88,7 @@ function ResponsiveDrawer(props) {
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" noWrap component="div">
-                        Responsive drawer
+                        數位學習平台
                     </Typography>
                 </Toolbar>
             </AppBar>
@@ -125,43 +130,14 @@ function ResponsiveDrawer(props) {
             >
                 <Toolbar />
                 <Typography paragraph>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-                    enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-                    imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-                    Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-                    Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-                    nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-                    leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-                    feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-                    consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-                    sapien faucibus et molestie ac.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus nonenim praesent elementum facilisis leo vel. Risus at ultrices mi tempusimperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentumleo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortisfeugiat vivamus at augue. At augue eget arcu dictum varius duis atconsectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massasapien faucibus et molestie ac.
                 </Typography>
                 <Typography paragraph>
-                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-                    eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-                    neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-                    tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-                    sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-                    tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-                    gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-                    et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-                    tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-                    eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-                    posuere sollicitudin aliquam ultrices sagittis orci a.
+                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorpereget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enimneque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverratellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisissed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbitincidunt ornare massa eget egestas purus viverra accumsan in. In hendreritgravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam semet tortor. Habitant morbi tristique senectus et. Adipiscing elit duistristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quiseleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nullaposuere sollicitudin aliquam ultrices sagittis orci a.
                 </Typography>
             </Box>
         </Box>
     );
 }
-
-ResponsiveDrawer.propTypes = {
-    /**
-     * Injected by the documentation to work in an iframe.
-     * You won't need it on your project.
-     */
-    window: PropTypes.func,
-};
 
 export default ResponsiveDrawer;
